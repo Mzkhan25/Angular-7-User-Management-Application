8f2aba9f2b5e327ef013e2f1b780b850a4abd0a6
Muiz Junaid Khan <mzkhan25@hotmail.com>
2018-10-28 18:20:08 +0500

    Server : Basic NetJs placed

--- Client/user-management/src/app/dashboard/dashboard.component.ts
+++ Client/user-management/src/app/dashboard/dashboard.component.ts
@@ -10,10 +10,10 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   users = [
-      {id: 1, firstName: 'Dhiraj', lastName: 'Ray', email: 'dhiraj@gmail.com', city:'Lahore'},
-      {id: 1, firstName: 'Tom', lastName: 'Jac', email: 'Tom@gmail.com', city:'Lahore'},
-      {id: 1, firstName: 'Hary', lastName: 'Pan', email: 'hary@gmail.com', city:'Karachi'},
-      {id: 1, firstName: 'praks', lastName: 'pb', email: 'praks@gmail.com' ,city:'Islamabad'},
+      {id: 1, firstName: 'Dhiraj', lastName: 'Ray', email: 'dhiraj@gmail.com', city:'Lahore', age:45},
+      {id: 1, firstName: 'Tom', lastName: 'Jac', email: 'Tom@gmail.com', city:'Lahore',age:78},
+      {id: 1, firstName: 'Hary', lastName: 'Pan', email: 'hary@gmail.com', city:'Karachi',age:7},
+      {id: 1, firstName: 'praks', lastName: 'pb', email: 'praks@gmail.com' ,city:'Islamabad',age:20},
     ];
   
     
