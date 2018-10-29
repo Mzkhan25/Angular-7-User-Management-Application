48d14f2ffffcd6a1f5b76e5cb9130ee8594ef2cd
Muiz Junaid Khan <mzkhan25@hotmail.com>
2018-10-29 20:47:23 +0500

    Server : Swagger for Api Documentation implemented

--- server/dist/main.js
+++ server/dist/main.js
@@ -10,14 +10,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = yield core_1.NestFactory.create(app_module_1.AppModule);
+        const hostDomain = app_module_1.AppModule.isDev ? `${app_module_1.AppModule.host}:${app_module_1.AppModule.port}` : app_module_1.AppModule.host;
+        const swaggerOptions = new swagger_1.DocumentBuilder()
+            .setTitle('Nest MEAN')
+            .setDescription('API Documentation')
+            .setVersion('1.0.0')
+            .setHost(hostDomain.split('//')[1])
+            .setSchemes(app_module_1.AppModule.isDev ? 'http' : 'https')
+            .setBasePath('/api')
+            .addBearerAuth('Authorization', 'header')
+            .build();
+        const swaggerDoc = swagger_1.SwaggerModule.createDocument(app, swaggerOptions);
+        app.use('/api/docs/swagger.json', (req, res) => {
+            res.send(swaggerDoc);
+        });
+        swagger_1.SwaggerModule.setup('/api/docs', app, null, {
+            swaggerUrl: `${hostDomain}/api/docs/swagger.json`,
+            explorer: true,
+            swaggerOptions: {
+                docExpansion: 'list',
+                filter: true,
+                showRequestDuration: true,
+            },
+        });
         if (module.hot) {
             module.hot.accept();
             module.hot.dispose(() => app.close());
         }
-        yield app.listen(3000);
+        app.setGlobalPrefix('api');
+        yield app.listen(app_module_1.AppModule.port);
     });
 }
 bootstrap();
---
+++ server/dist/shared/configuration/comfiguration.enum.d.ts
@@ -0,0 +1,5 @@
+export declare enum Configuration {
+    HOST = "HOST",
+    PORT = "PORT",
+    MONGO_URI = "MONGO_URI"
+}
