75bde900e895a28a4db8249ba00eba33a7f54a3a
Muiz Junaid Khan <mzkhan25@hotmail.com>
2019-01-31 17:14:58 +0500

    USer defined information from Get

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,7 @@ var async = require('async');
 // Display list of all users.
 exports.users_list = function(req, res, next) {
 
-    User.find()
+    User.find({},'first_name last_name user_name address job salary ')
       .sort([['id', 'ascending']])
       .exec(function (err, list_users) {
         if (err) { return next(err); }
